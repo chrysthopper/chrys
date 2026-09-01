b266e4f240ae3e4fa99408cdf534ddf3ce639dbb
chrysthopper <chrysthopper.souza@escola.pr.gov.br>
2026-09-01 09:23:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-      textos[j].classList.remove("ativo");
+console.log(botoes)
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
     }
-
-    botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
-  };
+    numeroSenha.textContent = tamanhoSenha;
 }
-
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-11-30");
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-  let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60);
-  let horas = Math.floor(minutos / 60);
-  let dias = Math.floor(horas / 24);
-
-  segundos %= 60;
-  minutos %= 60;
-  horas %= 24;
-  
-  return (
-  dias +
-  " dias " +
-  horas +
-  " horas " +
-  minutos +
-  " minutos " +
-  segundos +
-  " segundos"
-);
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
